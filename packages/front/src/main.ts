455cd5bca6f1a338cc0c017ce748a42ec70b5616
kelvindesaeger <kelvin.de.saeger@student.howest.be>
2023-09-26 12:36:50 +0200

    add firebase login register ...

--- packages/front/src/main.ts
+++ packages/front/src/main.ts
@@ -6,8 +6,18 @@ import 'virtual:uno.css'
 import App from './App.vue'
 import router from './router'
 
+import useFirebase from './composables/useFirebase'
+
 const app = createApp(App)
+const { restoreUser } = useFirebase()
 
-app.use(router)
+// app.use(router)
 
-app.mount('#app')
+// app.mount('#app')
+
+;(async () => {
+    await restoreUser()
+  
+    app.use(router)
+    app.mount('#app')
+  })()
--- packages/front/src/views/home.vue
+++ packages/front/src/views/home.vue
@@ -1,3 +1,36 @@
 <template>
-    Hello 👋🏼
-</template>
+    <div>
+      Hello 👋🏼
+      <p v-if="firebaseUser">Logged in user: {{ firebaseUser.email }}</p>
+        <button v-if="firebaseUser" @click="logout">Logout</button>
+      <RouterLink
+        v-else
+        class="mt-3 inline-block rounded text-center text-sm text-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-200"
+        to="/auth/login"
+      >
+        Login
+      </RouterLink>
+    </div>
+  </template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import { type AuthError } from 'firebase/auth'
+
+import useFirebase from '@/composables/useFirebase'
+
+export default {
+  setup() {
+    // Composables
+    const { logout, firebaseUser } = useFirebase()
+
+    
+
+    return {
+      firebaseUser,
+      logout,
+
+    }
+  },
+}
+</script>
