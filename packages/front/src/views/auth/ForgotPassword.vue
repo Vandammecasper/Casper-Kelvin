83380f605b909daffc5871e94647a1ac5273c55b
Casper Van Damme <casper.van.damme@student.howest.be>
2023-09-27 17:13:03 +0200

    forgot page finished, cross added in all auth

--- packages/front/src/views/auth/ForgotPassword.vue
+++ packages/front/src/views/auth/ForgotPassword.vue
@@ -1,3 +1,77 @@
 <template>
-    <div>FForgotPassword</div>
+    <form @submit.prevent="handleResetPassword" class="w-screen h-screen grid">
+        <div class="grid bg-neutral-800 place-self-center p-4">
+            <RouterLink to="/" class="justify-self-end">
+                <img src="../../../assets/No.svg" alt="">
+            </RouterLink>
+            <div class=" py-4 px-16">
+                <h1 class="text-4xl font-bold tracking-wider justify-self-center text-yellow-600">RESET PASSWORD {{ firebaseUser?.uid }}
+                </h1>
+                <div v-if="error">
+                    <p class="text-red-600">{{ error.message }}</p>
+                </div>
+
+                <div class="mt-6 place-self-center w-full">
+                    <label for="email" class="text-md block font-semibold tracking-wider text-white dark:text-gray-200">
+                        Email address
+                    </label>
+                    <input type="email" name="email" id="email" placeholder="youremail@example.com"
+                        class="mt-1 w-full border-3 text-white bg-neutral-800 border-neutral-500 p-2 focus:outline-none focus-visible:ring-2 focus-visible:border-yellow-600 focus-visible:ring-yellow-600"
+                        v-model="email" />
+                </div>
+
+                <button
+                    class="mt-6 w-full  border-2 border-yellow-600 bg-yellow-600 py-2 px-4 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus-visible:border-yellow-600 focus-visible:bg-yellow-700 focus-visible:ring-2 focus-visible:ring-yellow-300">
+                    RESET PASSWORD
+                </button>
+                <div class="flex justify-center">
+                    <RouterLink
+                        class="mt-1 inline-block rounded text-sm text-yellow-600 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-200 dark:text-yellow-200"
+                        to="/auth/login">
+                        Remembered your password?
+                    </RouterLink>
+                </div>
+            </div>
+            
+        </div>
+    </form>
 </template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import { type AuthError } from 'firebase/auth'
+import router from '@/router'
+
+import useFirebase from '@/composables/useFirebase'
+import { X } from 'lucide-vue-next'
+
+export default {
+    components: { X },
+
+    setup() {
+        // Composables
+        const { resetPassword } = useFirebase()
+        const email = ref<string>('')
+        const error = ref<AuthError | null>(null)
+        const sendEmail = ref<boolean>(false)
+
+        const handleResetPassword = () => {
+            resetPassword(email.value).then(() => {
+                // TODO: show notification
+                sendEmail.value = true
+            }).then(() => {
+                router.push('/auth/login')
+            })
+
+        }
+
+        return {
+            email,
+            error,
+            sendEmail,
+
+            handleResetPassword,
+        }
+    },
+}
+</script>
--- packages/front/src/views/auth/Login.vue
+++ packages/front/src/views/auth/Login.vue
@@ -1,60 +1,67 @@
 <template>
   <form @submit.prevent="handleLogin" class="w-screen h-screen grid">
-    <div class="grid bg-neutral-800 py-12 px-24 place-self-center">
-      <h1 class="text-4xl font-bold tracking-wider justify-self-center text-yellow-600">LOGIN {{ firebaseUser?.uid }}</h1>
-      <div v-if="error">
-        <p class="text-red-600">{{ error.message }}</p>
-      </div>
+    <div class="grid bg-neutral-800 p-4 place-self-center">
+      <RouterLink to="/" class="justify-self-end">
+        <img src="../../../assets/No.svg" alt="">
+      </RouterLink>
+      <div class=" py-4 px-16 grid">
+        <h1 class="text-4xl font-bold tracking-wider justify-self-center text-yellow-600">LOGIN {{ firebaseUser?.uid }}</h1>
+        <div v-if="error">
+          <p class="text-red-600">{{ error.message }}</p>
+        </div>
 
-      <div class="mt-6">
-        <label
-          for="email"
-          class="text-md block font-semibold tracking-wider text-white dark:text-gray-200"
-        >
-          Email address
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          class="mt-1 block border-3 text-white bg-neutral-800 border-neutral-500 p-2 focus:outline-none focus-visible:ring-2 focus-visible:border-yellow-600 focus-visible:ring-yellow-600"
-          v-model="loginCredentials.email"
-        />
-      </div>
-      <div class="mt-6">
-        <label
-          for="password"
-          class="text-md block font-semibold tracking-wider text-white dark:text-gray-200"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          class="mt-1 block border-3 text-white bg-neutral-800 border-neutral-500 p-2 focus:outline-none focus-visible:ring-2 focus-visible:border-yellow-600 focus-visible:ring-yellow-600"
-          v-model="loginCredentials.password"
-        />
-        <RouterLink
-          to="/auth/forgotpassword"
-          class="mt-1 inline-block rounded text-sm text-yellow-600 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-200 dark:text-yellow-200"
-        >
-          Forgot password?
-        </RouterLink>
-      </div>
+        <div class="mt-6">
+          <label
+            for="email"
+            class="text-md block font-semibold tracking-wider text-white dark:text-gray-200"
+          >
+            Email address
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="youremail@example.com"
+            class="mt-1 block border-3 text-white bg-neutral-800 border-neutral-500 p-2 focus:outline-none focus-visible:ring-2 focus-visible:border-yellow-600 focus-visible:ring-yellow-600"
+            v-model="loginCredentials.email"
+          />
+        </div>
+        <div class="mt-6">
+          <label
+            for="password"
+            class="text-md block font-semibold tracking-wider text-white dark:text-gray-200"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Your password"
+            class="mt-1 block border-3 text-white bg-neutral-800 border-neutral-500 p-2 focus:outline-none focus-visible:ring-2 focus-visible:border-yellow-600 focus-visible:ring-yellow-600"
+            v-model="loginCredentials.password"
+          />
+          <RouterLink
+            to="/auth/forgotpassword"
+            class="mt-1 inline-block rounded text-sm text-yellow-600 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-200 dark:text-yellow-200"
+          >
+            Forgot password?
+          </RouterLink>
+        </div>
 
-      <button
-        class="mt-6 w-full  border-2 border-yellow-600 bg-yellow-600 py-2 px-4 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus-visible:border-yellow-600 focus-visible:bg-yellow-700 focus-visible:ring-2 focus-visible:ring-yellow-300"
-      >
-        LOGIN
-      </button>
-      <div class="flex justify-center">
-        <RouterLink
-          class="mt-1 inline-block rounded text-sm text-yellow-600 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-200 dark:text-yellow-200"
-          to="/auth/register"
+        <button
+          class="mt-6 w-full  border-2 border-yellow-600 bg-yellow-600 py-2 px-4 font-semibold text-white hover:bg-yellow-700 focus:outline-none focus-visible:border-yellow-600 focus-visible:bg-yellow-700 focus-visible:ring-2 focus-visible:ring-yellow-300"
         >
-          Don't have an account?
-        </RouterLink>
+          LOGIN
+        </button>
+        <div class="flex justify-center">
+          <RouterLink
+            class="mt-1 inline-block rounded text-sm text-yellow-600 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-200 dark:text-yellow-200"
+            to="/auth/register"
+          >
+            Don't have an account?
+          </RouterLink>
+        </div>
       </div>
     </div>
   </form>
