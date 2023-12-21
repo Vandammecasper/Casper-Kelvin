a0da9bb726d37f6bbcdb08507ea74b275b8a4494
Casper Van Damme <casper.van.damme@student.howest.be>
2023-12-21 13:24:54 +0100

    everything but barber settings responsive

--- packages/front/src/components/headerAccount.vue
+++ packages/front/src/components/headerAccount.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="h-24 w-screen bg-black p-4 px-16 flex justify-between absolute top-0 z-50">
+    <div class="h-24 w-screen bg-black p-4 max-sm:px4 px-8 lg:px-16 flex justify-between absolute top-0 z-50">
         <div>
         <RouterLink to="/">
             <img src="/logo-PhotoRoom.png" alt="logo" class="h-16">
         </RouterLink>
         </div>
-        <div class="flex gap-16 items-center">
+        <div class="flex gap-4 md:gap-16 items-center">
             <RouterLink v-if="customUser?.role == 'SUPER_ADMIN'" to="/account/settings">
-                <h1>{{ $t('navigation.barberSettings') }}</h1>
+                <h1 class="max-sm:text-sm text-center">{{ $t('navigation.barberSettings') }}</h1>
             </RouterLink>
-            <RouterLink v-if="customUser?.role == 'SUPER_ADMIN'" to="/account/dashboard">
-                <h1>{{ $t('navigation.barberAnalytics') }}</h1>
+            <RouterLink v-if="customUser?.role == 'SUPER_ADMIN'" to="/account/dashboard" class="max-lg:hidden">
+                <h1 class="text-center">{{ $t('navigation.barberAnalytics') }}</h1>
             </RouterLink>
-            <RouterLink to="/account/myaccount">
+            <RouterLink v-if="customUser?.role == 'SUPER_ADMIN'" to="/account/myaccount">
                 <div class="h-16 w-16 bg-yellow-600 rounded-full flex items-center justify-center">
                     <img src="/Profile.svg" alt="profile icon" class="h-10 w-10 mb-1">
                 </div>
